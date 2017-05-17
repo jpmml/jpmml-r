4253f2cc236e2bd4c6f12fed0d46732eb84e69fb
Villu Ruusmann <villu.ruusmann@gmail.com>
2017-05-17 12:38:08 +0300

    Updated r2pmml package

--- src/test/R/xgboost.R
+++ src/test/R/xgboost.R
@@ -13,13 +13,8 @@ auto.fmap = genFMap(auto_X)
 auto.dmatrix = genDMatrix(auto_y, auto_X)
 
 generateXGBoostAutoNA = function(){
-	schema = list()
-	schema$response_name = "mpg"
-	schema$missing = -999
-
 	auto.xgboost = xgboost(data = auto.dmatrix, missing = -999, objective = "reg:linear", nrounds = 15)
-	auto.xgboost$fmap = auto.fmap
-	auto.xgboost$schema = schema
+	auto.xgboost = decorate(auto.xgboost, fmap = auto.fmap, response_name = "mpg", missing = -999)
 
 	mpg = predict(auto.xgboost, newdata = auto.dmatrix, missing = -999)
 
@@ -43,18 +38,13 @@ iris.fmap = genFMap(iris_X)
 iris.dmatrix = genDMatrix(iris_y, iris_X)
 
 generateXGBoostIris = function(){
-	schema = list()
-	schema$response_name = "Species"
-	schema$response_levels = c("setosa", "versicolor", "virginica")
-
 	iris.xgboost = xgboost(data = iris.dmatrix, missing = NA, objective = "multi:softprob", num_class = 3, nrounds = 15)
-	iris.xgboost$fmap = iris.fmap
-	iris.xgboost$schema = schema
+	iris.xgboost = decorate(iris.xgboost, fmap = iris.fmap, response_name = "Species", response_levels = c("setosa", "versicolor", "virginica"))
 
 	probabilities = predict(iris.xgboost, newdata = iris.dmatrix)
 	probabilities = matrix(probabilities, ncol = 3, byrow = TRUE)
 	species = max.col(probabilities)
-	species = schema$response_levels[species]
+	species = iris.xgboost$schema$response_levels[species]
 
 	storeRds(iris.xgboost, "XGBoostIris")
 	storeCsv(data.frame("Species" = species, "probability(setosa)" = probabilities[, 1], "probability(versicolor)" = probabilities[, 2], "probability(virginica)" = probabilities[, 3], check.names = FALSE), "XGBoostIris")
